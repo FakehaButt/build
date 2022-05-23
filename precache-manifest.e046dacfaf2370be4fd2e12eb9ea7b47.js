b633151b8fb4794dae9e1e9858461b75135440df
Fakeha Butt <fakehabutt9@gmail.com>
2022-05-24 01:52:03 +0500

    first commit

---
+++ precache-manifest.e046dacfaf2370be4fd2e12eb9ea7b47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "68d7f9c8ce5421b0091c",
+    "url": "/static/css/main.b697a44f.chunk.css"
+  },
+  {
+    "revision": "68d7f9c8ce5421b0091c",
+    "url": "/static/js/main.68d7f9c8.chunk.js"
+  },
+  {
+    "revision": "af08dc77e4e76da41e54",
+    "url": "/static/js/1.af08dc77.chunk.js"
+  },
+  {
+    "revision": "b379251ef391182c8c6e",
+    "url": "/static/js/2.b379251e.chunk.js"
+  },
+  {
+    "revision": "a5627a9c6f1e58df88af",
+    "url": "/static/js/runtime~main.a5627a9c.js"
+  },
+  {
+    "revision": "71efd238f3aed7ec9fe7024cc8670991",
+    "url": "/index.html"
+  }
+];
